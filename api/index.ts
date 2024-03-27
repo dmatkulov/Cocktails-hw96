f009512d0e16ba68195d6c21eb7d34ce745c257c
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-03-27 15:33:46 +0600

    Created User model, user route, added auth, permit and user middlewares

---
+++ api/index.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+
+import config from './config';
+import usersRouter from './routers/users';
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+
+app.use('/users', usersRouter);
+
+const run = async () => {
+  await mongoose.connect(config.mongoose.db);
+  
+  app.listen(port, () => {
+    console.log('Server online on port ' + port);
+  });
+  
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+void run();
